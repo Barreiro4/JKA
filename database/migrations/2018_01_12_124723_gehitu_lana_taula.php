c65030ca0c1bf5cb6f95d0f2bcff6099868b3227
Gaizka Izaguirre <dw3-2-07.17@uni.eus>
2018-01-16 13:00:53 +0000

    lana1negin

---
+++ database/migrations/2018_01_12_124723_gehitu_lana_taula.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class GehituLanaTaula extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lana', function (Blueprint $table) {
+            $table->increments('lana_id');
+            $table->string('izena');
+            $table->integer('langile_id')->unsigned();
+            $table->integer('makina_id')->unsigned();
+            
+            $table->foreign('langile_id')->references('langile_id')->on('langileak');
+            $table->foreign('makina_id')->references('makina_id')->on('makina');
+            
+            $table->timestamps();
+        });
+        
+        
+        //Schema::create('lana', function (Blueprint $table) {
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('lana');
+    }
+}
